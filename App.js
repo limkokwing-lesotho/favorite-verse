b257098a1092a450483b5b11f6b66bafb8b6dfa1
Ntholi Nkhatho <ntholi@live.com>
2022-05-26 13:39:07 +0200

    Use api to get bible verses

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+import React, { useState } from 'react';
+import VerseList from './VerseList';
+import { Entypo } from '@expo/vector-icons';
+import axios from 'axios';
 
 export default function App() {
+  const [input, setInput] = useState('');
+  const [verses, setVerses] = useState([]);
+
+  function addVerse() {
+    axios.get('https://bible-api.com/' + input).then((response) => {
+      setVerses([...verses, response.data]);
+      setInput('');
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text style={styles.title}>Bible Verse</Text>
+        <View style={styles.controls}>
+          <TextInput
+            style={styles.textInput}
+            value={input}
+            onChangeText={setInput}
+          />
+          <TouchableOpacity style={styles.btn} onPress={addVerse}>
+            <Entypo name='add-to-list' size={22} color='white' />
+          </TouchableOpacity>
+        </View>
+        <VerseList verses={verses} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EEEEEE',
+  },
+  title: {
+    backgroundColor: '#1877F2',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '500',
+    padding: 15,
+  },
+  controls: {
+    marginTop: 20,
+    marginHorizontal: 20,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#1877F2',
+    color: '#fff',
+    padding: 10,
+    marginLeft: 10,
+    borderRadius: 5,
+    flexDirection: 'row',
+  },
+  btnText: {
+    color: '#fff',
+    textTransform: 'uppercase',
+    fontWeight: '500',
+    marginLeft: 5,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff',
+    flex: 4,
+    borderRadius: 3,
+    padding: 7,
+    marginRight: 10,
   },
 });
---
+++ VerseList.js
@@ -0,0 +1,46 @@
+import { StyleSheet, View, Text } from 'react-native';
+import React from 'react';
+
+export default function VerseList({ verses }) {
+  function transform(item) {
+    return (
+      <View style={styles.card} key={item.reference}>
+        <Text>{item.text}</Text>
+        <Text style={styles.reference}>{item.reference}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      {verses.map((verse) => transform(verse))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 20,
+    padding: 10,
+    borderColor: '#999',
+    borderBottomWidth: 0,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+  },
+  card: {
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: '#CFD8DC',
+    borderLeftWidth: 5,
+    borderLeftColor: '#90A4AE',
+  },
+  reference: {
+    fontSize: 13,
+    color: '#999',
+    textAlign: 'right',
+    fontStyle: 'italic',
+  },
+});
